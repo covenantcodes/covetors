c5b7f383cb88b673249304a68fe000b5c2b7d0ab
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2023-10-10 23:07:00 +0100

    Created header navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import './App.css'
+import "./App.css";
+import CustomButton from "./Components/Button";
 
-const App = () =>{
+const App = () => {
   return (
-    <div className='main_pheader'>
-        This is main header
+    <div className="landing_page_main">
+      <div className="landing_page">
+        <div className="header_nav">
+          <div className="logo_box">
+            <img src="../img/icon.png" alt="" className="logo" />
+          </div>
+          <div className="links">
+            <ul>
+              <li>Courses</li>
+              <li>About Us</li>
+              <li>Teachers</li>
+              <li>Pricing</li>
+              <li>Careers</li>
+            </ul>
+          </div>
+          <CustomButton
+            textColor="white"
+            bgColor="#EC884D"
+            text="Get Started"
+            width="100px"
+            padding="10px"
+            fontWeight="bold"
+            hoverTextColor="white"
+            hoverBgColor="#E47331"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/Button.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+interface CustomButtonProps {
+  textColor: string;
+  bgColor: string;
+  text: string;
+  width: string;
+  padding: string;
+  fontWeight: string;
+  hoverTextColor: string;
+  hoverBgColor: string;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  textColor,
+  bgColor,
+  text,
+  width,
+  padding,
+  fontWeight,
+  hoverTextColor,
+  hoverBgColor,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const buttonStyle: React.CSSProperties = {
+    color: isHovered ? hoverTextColor : textColor,
+    backgroundColor: isHovered ? hoverBgColor : bgColor,
+    width: width,
+    padding: padding,
+    fontWeight: fontWeight,
+    borderRadius: '73px',
+    border: 'none',
+    cursor: 'pointer',
+    transition: 'all 0.3s',
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <button
+      style={buttonStyle}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default CustomButton;
